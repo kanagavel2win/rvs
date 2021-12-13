d5efea3fa74766315ce3d3a1b3bd65f704169e20
kanagavel <kanagu2win@gmail.com>
2021-12-13 20:05:57 +0530

    packageName change

---
+++ rvs/src/main/java/com/rvs/springboot/thymeleaf/controller/HomeController.java
@@ -0,0 +1,155 @@
+package com.rvs.springboot.thymeleaf.controller;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.rvs.springboot.thymeleaf.dao.UserRepository;
+import com.rvs.springboot.thymeleaf.entity.User;
+
+@Controller
+
+public class HomeController {
+
+	
+	@Autowired
+	private UserRepository userRepository;
+
+	/*@ModelAttribute
+	public void addAttributes(Model themodel, HttpSession session, HttpServletRequest request) {
+
+		String dataLoginEmailID = "";
+		String dataLoginClubID = "";
+		try {
+
+			try {
+				if (request.getSession().getAttribute("dataLoginEmailID").toString().equals(null)) {
+					dataLoginClubID = getLoginClubID();
+					request.getSession().setAttribute("dataLoginClubID", dataLoginClubID);
+					dataLoginEmailID = getLoginemailID();
+					request.getSession().setAttribute("dataLoginEmailID", dataLoginEmailID);
+				}
+			} catch (NullPointerException e) {
+				dataLoginClubID = getLoginClubID();
+				request.getSession().setAttribute("dataLoginClubID", dataLoginClubID);
+				dataLoginEmailID = getLoginemailID();
+				request.getSession().setAttribute("dataLoginEmailID", dataLoginEmailID);
+			}
+
+			dataLoginEmailID = request.getSession().getAttribute("dataLoginEmailID").toString();
+			dataLoginClubID = request.getSession().getAttribute("dataLoginClubID").toString();
+
+		} catch (Exception e) {
+
+		} finally {
+			themodel.addAttribute("dataLoginEmailID", dataLoginEmailID);
+			themodel.addAttribute("dataLoginClubID", dataLoginClubID);
+		}
+
+	}
+*/
+	@GetMapping("/")
+	public String home(Model theModel, HttpSession session, HttpServletRequest request) {
+		return "index";
+/*		if (logintype("ROLE_MEMBER")) {
+
+			theModel.addAttribute("MemberID", getLoginMemberID());
+			return "indexMember";
+		} else if (logintype("ROLE_CLUBADMIN")) {
+			return "index";
+		} else {
+			return "redirect:logout";
+		}*/
+	}
+
+	private boolean logintype(String expectedrole) {
+
+		@SuppressWarnings("unchecked")
+		List<SimpleGrantedAuthority> authorities = (List<SimpleGrantedAuthority>) SecurityContextHolder.getContext()
+				.getAuthentication().getAuthorities();
+
+		boolean RoleStatus = false;
+
+		for (SimpleGrantedAuthority simpleGrantedAuthority : authorities) {
+
+			if (simpleGrantedAuthority.getAuthority().toString().contains(expectedrole)) {
+				RoleStatus = true;
+			}
+		}
+
+		return RoleStatus;
+
+	}
+
+	public String getLoginemailID() {
+
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		String currentPrincipalName = authentication.getName();
+		User user2 = userRepository.findByEmail(currentPrincipalName);
+		return user2.getEmail();
+
+	}
+
+	public String getLoginMemberID() {
+
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		String currentPrincipalName = authentication.getName();
+		User user2 = userRepository.findByEmail(currentPrincipalName);
+		return user2.getmemberID();
+
+	}
+
+	public String getLoginClubID() {
+
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		String currentPrincipalName = authentication.getName();
+		User user2 = userRepository.findByEmail(currentPrincipalName);
+		return user2.getClubID();
+
+	}
+
+	@GetMapping("/index")
+	public String index(Model theModel) {
+		return "index";
+		/*if (logintype("ROLE_MEMBER")) {
+			theModel.addAttribute("MemberID", getLoginMemberID());
+			return "indexMember";
+		} else if (logintype("ROLE_CLUBADMIN")) {
+			return "index";
+		} else {
+			return "redirect:logout";
+		}*/
+
+	}
+	@GetMapping("login")
+	public String login(Model model) {
+
+		return "login";
+	}
+
+	@GetMapping("403")
+	public String accessDenied(Model model) {
+
+		return "403";
+	}
+
+	@GetMapping("addnewbranch")
+	public String addnewbranch(Model theModel) {
+		return "branchadd";
+
+	}
+	@GetMapping("branchlist")
+	public String branchlist(Model theModel) {
+		return "branchlist";
+
+	}
+}

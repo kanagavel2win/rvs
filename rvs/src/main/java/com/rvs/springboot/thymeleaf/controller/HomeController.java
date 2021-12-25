e8083084af2950be22cc53581fb1663bd2cf600e
kanagavel <kanagu2win@gmail.com>
2021-12-25 09:25:37 +0530

    Calendar fist stage completed

--- rvs/src/main/java/com/rvs/springboot/thymeleaf/controller/HomeController.java
+++ rvs/src/main/java/com/rvs/springboot/thymeleaf/controller/HomeController.java
@@ -1,5 +1,9 @@
 package com.rvs.springboot.thymeleaf.controller;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -10,6 +14,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 
@@ -162,4 +167,73 @@ public class HomeController {
 	{
 		return "empjob";
 	}
+	
+	@GetMapping("empattendance")
+	public String empattendance(Model theModel, @RequestParam(value="date", defaultValue="", required=false) String attdate)
+	{
+		SimpleDateFormat formatterdate = new SimpleDateFormat("dd/MM/yyyy");
+		SimpleDateFormat formatteryear = new SimpleDateFormat("yyyy");
+		SimpleDateFormat formattermonth = new SimpleDateFormat("MM");
+		SimpleDateFormat formatterdd = new SimpleDateFormat("dd");
+		SimpleDateFormat formattermonname = new SimpleDateFormat("MMMM");
+		Date date = new Date();  
+		if (!attdate.equalsIgnoreCase(""))
+		{
+			try {
+				date =   formatterdate.parse(attdate);
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
+		}
+		String currentDate=formatterdate.format(date).toString();
+	    int currentyear=Integer.parseInt(formatteryear.format(date).toString());
+	    int currentmonth=Integer.parseInt(formattermonth.format(date).toString());
+	    int currentdd=Integer.parseInt(formatterdd.format(date).toString());
+	    String currentmonname=formattermonname.format(date).toString();
+	    Calendar calendar = Calendar.getInstance();
+	    calendar.set(currentyear,currentmonth-1,1);
+	    String stringDay=calendar.getTime().toString().substring(0,3);
+	    int currentmonthnumDays = calendar.getActualMaximum(Calendar.DATE);
+	    int firstdayofmonth=1;
+	    switch(stringDay)
+	    {
+	    case "Sun": 	firstdayofmonth=1; break;
+	    case "Mon": 	firstdayofmonth=2; break;
+	    case "Tue": 	firstdayofmonth=3; break;
+	    case "Wed": 	firstdayofmonth=4; break;
+	    case "Thu": 	firstdayofmonth=5; break;
+	    case "Fri": 	firstdayofmonth=6; break;
+	    case "Sat": 	firstdayofmonth=7; break;
+	    }
+	    
+	    String calhtml="<tr>";
+	    
+	    for(int i=1;i<firstdayofmonth;i++)
+	    {
+	    	calhtml=calhtml+"<td></td>";
+	    }
+	    
+	    for(int i=1;i<=currentmonthnumDays;i++)
+	    {
+	    	if((firstdayofmonth+i-2)%7 ==0)
+	    	{
+	    		calhtml=calhtml+"</tr><tr>";
+	    	}
+	    	
+	    	if(currentdd==i)
+	    	{
+	    		calhtml=calhtml+"<td class='selectdate' name='"+ (i) +"/"+ (currentmonth) +"/"+ (currentyear) +"'>"+ i+ "</td>";
+	    	}else
+	    	{
+	    	calhtml=calhtml+"<td class='td' name='"+ (i) +"/"+ (currentmonth) +"/"+ (currentyear) +"'>"+ i+ "</td>";
+	    	}
+	    }
+	    calhtml=calhtml+"</tr>";
+	    
+	    theModel.addAttribute("currentyear", currentyear);
+	    theModel.addAttribute("currentmonname", currentmonname);
+	    theModel.addAttribute("calhtml", calhtml);
+		return "empattendance";
+	}
+	
 }
